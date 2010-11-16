f73d02f27f8a73ff4662c95d9e67363ed28753b5
Austin Fagan <austinfagan@Austin-Fagans-MacBook-Pro.local>
2010-11-16 12:22:28 +0200

    Adding the initial app with basic question and answer scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FaqYou::Application.config.secret_token = 'f9d079ea948ebd619a179e792754076153fe550592bc7cedff4ecd73d3df94805e9800a67e7d0c8532fbdc95f28b24f99ad14a26dd242b22dcee42af5b5ac423'
